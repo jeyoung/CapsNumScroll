7a0b1e807210a0e1304e8546b29ab0ccf9b74d26
Eddy Young <jeyoung@priscimon.com>
2023-08-01 10:33:01 +0100

    Support multiple screens

--- src/CapsNumScroll.cpp
+++ src/CapsNumScroll.cpp
@@ -1,5 +1,5 @@
-#include "resource.h"
 #include <windows.h>
+#include "resource.h"
 
 #define APPLICATION_NAME "CapsNumScroll"
 
@@ -11,13 +11,18 @@
 #define NOTIFY_ICON_EVENT WM_USER + 0x1011
 #define MENU_QUIT WM_USER + 0x1012
 
+#define MONITORS_LIMIT 128
+
 static HINSTANCE hMainInstance;
-static HWND hMainWindow;
 static NOTIFYICONDATA notifyIconData;
-static RECT primaryMonitorRect;
+
 static HMENU hMainMenu;
 static HBITMAP hBitmap;
 static DWORD resourceId;
+
+static UINT monitorcounter;
+static RECT monitorrects[MONITORS_LIMIT];
+static HWND hWindows[MONITORS_LIMIT];
 
 LRESULT CALLBACK
 WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -37,7 +42,7 @@ HideWindowTimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
 enum { CAPS_LOCK = 20, NUM_LOCK = 144, SCROLL_LOCK };
 
 VOID
-RegisterMainWindowClass()
+RegisterWindowClass()
 {
     WNDCLASSEX wndcls = { 0 };
     wndcls.cbSize = sizeof(WNDCLASSEX);
@@ -56,12 +61,16 @@ CreateApplicationMenu()
     AppendMenu(hMainMenu, MF_ENABLED | MF_STRING, MENU_QUIT, "E&xit");
 }
 
+/**
+ * Create a notification icon, linking it to the first window to receive
+ * notifications.
+ */
 VOID
 CreateNotificationIcon()
 {
     notifyIconData = { 0 };
     notifyIconData.cbSize = sizeof(NOTIFYICONDATA);
-    notifyIconData.hWnd = hMainWindow;
+    notifyIconData.hWnd = hWindows[0];
     strcpy(notifyIconData.szTip, APPLICATION_NAME);
     notifyIconData.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
     notifyIconData.uCallbackMessage = NOTIFY_ICON_EVENT;
@@ -70,23 +79,29 @@ CreateNotificationIcon()
 }
 
 VOID
-CreateMainWindow()
+CreateWindows()
 {
+    monitorcounter = 0;
     EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
 
-    int width = primaryMonitorRect.right - primaryMonitorRect.left;
-    hMainWindow = CreateWindowEx(WS_EX_NOACTIVATE | WS_EX_TOPMOST,
-                                 APPLICATION_NAME "Window",
-                                 APPLICATION_NAME,
-                                 WS_POPUP | WS_BORDER,
-                                 (int)(0.5 * (width - WINDOW_SIZE)),
-                                 (int)(primaryMonitorRect.bottom - (1.5 * WINDOW_SIZE)),
-                                 WINDOW_SIZE,
-                                 WINDOW_SIZE,
-                                 NULL,
-                                 NULL,
-                                 hMainInstance,
-                                 (LPVOID)NULL);
+    for (UINT i=0; i<monitorcounter; ++i) {
+	RECT rect = monitorrects[i];
+	int width = rect.right - rect.left;
+	int left = rect.left + (0.5*(width-WINDOW_SIZE));
+	int top = rect.bottom - (1.5*WINDOW_SIZE);
+	hWindows[i] = CreateWindowEx(WS_EX_NOACTIVATE | WS_EX_TOPMOST,
+		APPLICATION_NAME "Window",
+		APPLICATION_NAME,
+		WS_POPUP | WS_BORDER,
+		left,
+		top,
+		WINDOW_SIZE,
+		WINDOW_SIZE,
+		NULL,
+		NULL,
+		hMainInstance,
+		(LPVOID)NULL);
+    }
 }
 
 int CALLBACK
@@ -94,11 +109,13 @@ WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
     hMainInstance = hInstance;
 
-    RegisterMainWindowClass();
-    CreateMainWindow();
+    RegisterWindowClass();
+    CreateWindows();
 
-    ShowWindow(hMainWindow, SW_HIDE);
-    UpdateWindow(hMainWindow);
+    for (int i=0; i<monitorcounter; ++i) {
+	ShowWindow(hWindows[i], SW_HIDE);
+	UpdateWindow(hWindows[i]);
+    }
 
     DisplayKeyState(CAPS_LOCK);
 
@@ -108,10 +125,10 @@ WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
     HHOOK hook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, hInstance, 0);
 
     MSG messages;
-    while (GetMessage(&messages, hMainWindow, 0, 0))
+    while (GetMessage(&messages, NULL, 0, 0))
     {
-        TranslateMessage(&messages);
-        DispatchMessage(&messages);
+	TranslateMessage(&messages);
+	DispatchMessage(&messages);
     }
 
     UnhookWindowsHookEx(hook);
@@ -128,60 +145,60 @@ WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     switch (uMsg)
     {
-    case WM_CLOSE:
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    case WM_COMMAND:
-        if (LOWORD(wParam) == MENU_QUIT)
-            PostQuitMessage(0);
-        break;
-    case WM_PAINT:
-        {
-            hBitmap = LoadBitmap(hMainInstance, MAKEINTRESOURCE(resourceId));
+	case WM_CLOSE:
+	case WM_DESTROY:
+	    PostQuitMessage(0);
+	    break;
+	case WM_COMMAND:
+	    if (LOWORD(wParam) == MENU_QUIT)
+		PostQuitMessage(0);
+	    break;
+	case WM_PAINT:
+	    hBitmap = LoadBitmap(hMainInstance, MAKEINTRESOURCE(resourceId));
 
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hMainWindow, &ps);
+	    for (int i=0; i<monitorcounter; ++i) {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWindows[i], &ps);
 
-            HDC hdcMem = CreateCompatibleDC(hdc);
-            HGDIOBJ oldBitmap = SelectObject(hdcMem, hBitmap);
+		HDC hdcMem = CreateCompatibleDC(hdc);
+		HGDIOBJ oldBitmap = SelectObject(hdcMem, hBitmap);
 
-            BITMAP bitmap;
-            GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+		BITMAP bitmap;
+		GetObject(hBitmap, sizeof(BITMAP), &bitmap);
 
-            SetStretchBltMode(hdc, HALFTONE);
-            StretchBlt(hdc,
-                       0, 0,
-                       WINDOW_SIZE, WINDOW_SIZE,
-                       hdcMem,
-                       0, 0, bitmap.bmWidth, bitmap.bmHeight,
-                       SRCCOPY);
+		SetStretchBltMode(hdc, HALFTONE);
+		StretchBlt(hdc,
+			0, 0,
+			WINDOW_SIZE, WINDOW_SIZE,
+			hdcMem,
+			0, 0, bitmap.bmWidth, bitmap.bmHeight,
+			SRCCOPY);
 
-            SelectObject(hdcMem, oldBitmap);
-            DeleteDC(hdcMem);
+		SelectObject(hdcMem, oldBitmap);
+		DeleteDC(hdcMem);
 
-            EndPaint(hMainWindow, &ps);
+		EndPaint(hWindows[i], &ps);
 
-            SetWindowLong(hMainWindow,
-                          GWL_EXSTYLE,
-                          GetWindowLong(hMainWindow, GWL_EXSTYLE) | WS_EX_LAYERED);
-            SetLayeredWindowAttributes(hMainWindow, 0, 255*0.9, LWA_ALPHA);
-        }
-        break;
-    case NOTIFY_ICON_EVENT:
-        if (LOWORD(lParam) == WM_RBUTTONUP)
-        {
-            POINT clickPosition;
-            GetCursorPos(&clickPosition);
-            TrackPopupMenuEx(hMainMenu,
-                             TPM_RIGHTALIGN | TPM_BOTTOMALIGN | TPM_LEFTBUTTON,
-                             clickPosition.x, clickPosition.y,
-                             hwnd,
-                             NULL);
-        }
-        break;
-    default:
-        result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		SetWindowLong(hWindows[i],
+			GWL_EXSTYLE,
+			GetWindowLong(hWindows[i], GWL_EXSTYLE) | WS_EX_LAYERED);
+		SetLayeredWindowAttributes(hWindows[i], 0, 255*0.9, LWA_ALPHA);
+	    }
+	    break;
+	case NOTIFY_ICON_EVENT:
+	    if (LOWORD(lParam) == WM_RBUTTONUP)
+	    {
+		POINT clickPosition;
+		GetCursorPos(&clickPosition);
+		TrackPopupMenuEx(hMainMenu,
+			TPM_RIGHTALIGN | TPM_BOTTOMALIGN | TPM_LEFTBUTTON,
+			clickPosition.x, clickPosition.y,
+			hwnd,
+			NULL);
+	    }
+	    break;
+	default:
+	    result = DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 
     return result;
@@ -191,12 +208,12 @@ LRESULT CALLBACK
 LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if (nCode < 0)
-        goto nextHook;
+	goto nextHook;
 
     if (wParam == WM_KEYUP)
     {
-        KBDLLHOOKSTRUCT *kbdhook = (KBDLLHOOKSTRUCT *) lParam;
-        DisplayKeyState(kbdhook->vkCode);
+	KBDLLHOOKSTRUCT *kbdhook = (KBDLLHOOKSTRUCT *) lParam;
+	DisplayKeyState(kbdhook->vkCode);
     }
 
 nextHook:
@@ -209,23 +226,23 @@ DisplayKeyState(DWORD keyCode)
     SHORT keyState = GetKeyState(keyCode) & 0x3;
     switch (keyCode)
     {
-    case CAPS_LOCK:
-        {
-            KillTimer(hMainWindow, HIDE_WINDOW_TIMER_EVENT);
-            InvalidateRgn(hMainWindow, NULL, TRUE);
-            if (keyState)
-            {
-                resourceId = CAPS_ON;
-                ShowWindow(hMainWindow, SW_SHOW);
-            }
-            else
-            {
-                resourceId = CAPS_OFF;
-                SetTimer(hMainWindow, HIDE_WINDOW_TIMER_EVENT, HIDE_WINDOW_TIMER_INTERVAL, HideWindowTimerProc);
-            }
-            UpdateWindow(hMainWindow);
-        }
-        break;
+	case CAPS_LOCK:
+	    for (int i=0; i<monitorcounter; ++i) {
+		KillTimer(hWindows[i], HIDE_WINDOW_TIMER_EVENT);
+		InvalidateRgn(hWindows[i], NULL, TRUE);
+		if (keyState)
+		{
+		    resourceId = CAPS_ON;
+		    ShowWindow(hWindows[i], SW_SHOW);
+		}
+		else
+		{
+		    resourceId = CAPS_OFF;
+		    SetTimer(hWindows[i], HIDE_WINDOW_TIMER_EVENT, HIDE_WINDOW_TIMER_INTERVAL, HideWindowTimerProc);
+		}
+		UpdateWindow(hWindows[i]);
+	    }
+	    break;
     }
 }
 
@@ -235,11 +252,7 @@ MonitorEnumProc(HMONITOR hMonitor, HDC, LPRECT, LPARAM)
     MONITORINFOEX info;
     info.cbSize = sizeof(MONITORINFOEX);
     GetMonitorInfo(hMonitor, &info);
-
-    RECT tmp = info.rcWork;
-    if (info.dwFlags)
-        primaryMonitorRect = tmp;
-
+    monitorrects[monitorcounter++] = info.rcWork;
     return TRUE;
 }
 
--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,5 @@
 @echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 mkdir ..\build
 rc CapsNumScroll.rc
 move CapsNumScroll.res ..\build
